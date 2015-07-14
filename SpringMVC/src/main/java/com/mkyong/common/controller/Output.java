907b9cbc90082d24c1c02ad61df2775008523f65
gulshan <agulshan@aconex.com>
2015-07-14 17:41:10 +0530

    temp

---
+++ SpringMVC/src/main/java/com/mkyong/common/controller/Output.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.mkyong.common.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ *
+ * @author agulshan
+ */
+public class Output implements Serializable{
+ private String name;
+ private String age;
+ private List<Certificate> certificates;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public List<Certificate> getCertificates() {
+        return certificates;
+    }
+
+    public void setCertificates(List<Certificate> certificates) {
+        this.certificates = certificates;
+    }
+    
+}
